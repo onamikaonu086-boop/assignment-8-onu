874924c66333a8d9e4fe25b74174bb6c6a745c71
onamikaonu086-boop <onamikaonu086@gmail.com>
2026-05-23 22:36:44 +0600

    image optimise off

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true, 
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'i.ibb.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      },
     ],
   },
 };
-
 export default nextConfig;
